0e3f8c1bf0c330ce3c80fd15ccdea3ca63458d26
Nathan Schultz <nschult@uwo.ca>
2025-09-05 10:42:34 -0400

    made death triggers better

--- scenes/Main/death_groups.gd
+++ scenes/Main/death_groups.gd
@@ -3,60 +3,60 @@ class_name DeathGroups
 
 @onready var main = get_parent()
 
-# each coin group is represented as an array
+# each death group is represented as an array
 # this array: [[0, 1, 0], [1, 1, 1], [0, 1, 0]]
-# would produce this coin pattern:
+# would produce this death pattern:
 # 0 
 #000
 # 0
-var coinGroups: Array[Array] = [
-	[[0, 1, 0], [1, 1, 1], [0, 1, 0]],
-	[[0, 1, 1, 1, 0, ], [1, 1, 1, 1, 1], [0, 1, 1, 1, 0]],
-	[[0, 0, 1, 0, 0], [0, 1, 0, 1, 0], [1, 1, 1, 1, 1]]
+var deathGroups: Array[Array] = [
+	[[0, 0, 1], [0, 1, 0], [1, 0, 0]],
+	[[1],[1],[1],[1],[1]],
+	[[1, 0, 0], [0, 1, 0], [0, 0, 1]],
 	]
 var rng: RandomNumberGenerator = RandomNumberGenerator.new()
 var triggerSize: Vector2
 const DEATH_TRIGGER : PackedScene = preload("res://scenes/Main/DeathTrigger.tscn")
-var activeCoins: Array[Node2D] = []
+var activeTriggers: Array[Node2D] = []
 
 func _ready():
-	#coinSize = getCoinSize()
+	#coinSize = getCoinSize() # leftover from copying the code from coins
 	triggerSize = Vector2(32, 32) # hardcoded for testing
 
 func _process(delta):
-	for coinIndex in range(activeCoins.size() - 1, -1, -1):
-		var currentCoin: Node2D = activeCoins[coinIndex]
+	for triggerIndex in range(activeTriggers.size() - 1, -1, -1):
+		var currentTrigger: Node2D = activeTriggers[triggerIndex]
 		
-		if !is_instance_valid(currentCoin):
-			activeCoins.remove_at(coinIndex)
+		if !is_instance_valid(currentTrigger):
+			activeTriggers.remove_at(triggerIndex)
 			continue
 		
-		currentCoin.position.x -= main.scrollSpeed * delta
+		currentTrigger.position.x -= main.scrollSpeed * delta
 		
-		if currentCoin.position.x + (triggerSize[0] / 2) < 0:
-			currentCoin.queue_free()
-			activeCoins.remove_at(coinIndex)
+		if currentTrigger.position.x + (triggerSize[0] / 2) < 0:
+			currentTrigger.queue_free()
+			activeTriggers.remove_at(triggerIndex)
 		
 
 func spawnTriggers():
 	# note: remove the underscores eventually. it's just to prevent warnings
-	var selection: int = rng.randi_range(0, coinGroups.size() - 1)
-	var coinPattern: Array = coinGroups[selection]
+	var selection: int = rng.randi_range(0, deathGroups.size() - 1)
+	var deathPattern: Array = deathGroups[selection]
 	@warning_ignore("narrowing_conversion")
 	var yPosition: int = rng.randi_range(0, get_viewport_rect().size.y) # spawn at a random y coord
 	
 	# n^2 runtime but i'm never going to make a crazy complex coin pattern so it's okay
-	for rowIndex in range(coinPattern.size()):
-		var row = coinPattern[rowIndex]
+	for rowIndex in range(deathPattern.size()):
+		var row = deathPattern[rowIndex]
 		for colIndex in range(row.size()):
-			var coinVal = row[colIndex]
-			if coinVal == 1:
-				var coinInstance = DEATH_TRIGGER.instantiate()
+			var deathVal = row[colIndex]
+			if deathVal == 1:
+				var deathInstance = DEATH_TRIGGER.instantiate()
 				var xPos = get_viewport_rect().size.x + (colIndex * triggerSize.x)
 				var yPos = yPosition + (rowIndex * triggerSize.y)
-				coinInstance.position = Vector2(xPos, yPos)
-				add_child(coinInstance)
-				activeCoins.append(coinInstance)
+				deathInstance.position = Vector2(xPos, yPos)
+				add_child(deathInstance)
+				activeTriggers.append(deathInstance)
 	
 # goes unused currently but will be implemented later
 #func getCoinSize() -> Vector2:
