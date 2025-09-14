658b8ed62aeea0afcad931f0378529d3483262b0
Nathan Schultz <nschult@uwo.ca>
2025-09-14 15:27:04 -0400

    title screen redesign; flashing start text and more

--- scenes/TitleScreen/title_screen_gui.gd
+++ scenes/TitleScreen/title_screen_gui.gd
@@ -2,11 +2,20 @@ extends Control
 
 @export var gameScenePath: String = "res://scenes/Main/Main.tscn"
 
+var radToTransparency := PI / 2
+
 func _ready():
 	
 	$StartButton.pressed.connect(_on_start_button_pressed)
+	
+func _process(_delta):
+	
+	radToTransparency += PI * _delta
+	
+	$StartInfo.modulate = Color(1, 1, 1, (sin(radToTransparency) + 1) / 2)
 
 func _on_start_button_pressed():
+	print("pressed!")
 	var titleScreen = get_parent()
 	Global.jetskiInfo = [titleScreen.get_node("DummyJetski").exportedPosition, titleScreen.get_node("DummyJetski").exportedVelocity]
 	Global.currentCoins = 0
