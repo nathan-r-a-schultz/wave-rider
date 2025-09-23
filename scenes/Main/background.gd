5c01d3c71452e90f139002098fa4e53d8fcf4cce
Nathan Schultz <nschult@uwo.ca>
2025-09-23 15:55:20 -0400

    clouds are now associated with the background parallax

--- scenes/Main/background.gd
+++ scenes/Main/background.gd
@@ -15,6 +15,7 @@ func _ready():
 	$Beach.autoscroll = Vector2(0.0, 0.0)
 	
 	cloudSize = Vector2(36, 18)
+	initClouds()
 	
 	if Global.titleInfo[0] != -1:
 		radToTransparency = Global.titleInfo[0]
@@ -23,15 +24,6 @@ func _ready():
 		$StartInfo.queue_free()
 
 func _process(_delta):
-	
-	scrollClouds(_delta)
-	
-	if main.scrollSpeed > 0:
-		cloudScrollAccum += main.scrollSpeed * _delta
-
-	if cloudScrollAccum >= cloudSpacing:
-		spawnClouds()
-		cloudScrollAccum = 0.0
 	
 	# to anyone reading this code: these if statements are sooooo janky
 	# i have no idea why the parallax layers speed up at the same rate as the water but don't do that for slowing down
@@ -69,30 +61,20 @@ func _process(_delta):
 			$StartInfo.queue_free()
 			Global.titleInfo = [-1]
 	
-func scrollClouds(_delta):
-	for cloudIndex in range(activeClouds.size() - 1, -1, -1):
-		var currentCloud: TextureRect = activeClouds[cloudIndex]
-		
-		if !is_instance_valid(currentCloud):
-			activeClouds.remove_at(cloudIndex)
-			continue
-		
-		currentCloud.position.x -= main.scrollSpeed * _delta
-		
-		if currentCloud.position.x < (-(get_viewport_rect().size.x - 320) * 2) - 36:
-			currentCloud.queue_free()
-			activeClouds.remove_at(cloudIndex)
-			
-func spawnClouds():
-	print("spawn")
-	var yPosition: int = rng.randi_range(0, 2) * 18 + 1 # spawn at a random y coord
-	var cloudImage: int = rng.randi_range(1, 3)
+func initClouds():
+	
+	var numClouds = 2
+	
+	for i in range(0, 3):
+		for j in range(numClouds):
+			var yPosition: int = i * 18 + 1
+			var cloudImage: int = rng.randi_range(1, 3)
 
-	var cloudInstance = TextureRect.new()
-	var xPos = get_viewport_rect().size.x
-	var yPos = yPosition
-	cloudInstance.texture = load("res://assets/clouds/cloud" + str(cloudImage) + ".png")
-	cloudInstance.position = Vector2(xPos, yPos)
-	add_child(cloudInstance)
-	activeClouds.append(cloudInstance)
+			var cloudInstance = TextureRect.new()
+			var xPos = int((get_viewport_rect().size.x / (numClouds + 1)) * (j + 1) - 18)
+			var yPos = yPosition
+			cloudInstance.texture = load("res://assets/clouds/cloud" + str(cloudImage) + ".png")
+			cloudInstance.position = Vector2(xPos, yPos)
+			$Sky.add_child(cloudInstance)
+		numClouds += 1
 	
