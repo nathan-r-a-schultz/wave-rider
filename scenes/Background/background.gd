8c77fc552eda22dc6fba390267b2aaf00fda0557
Natratz <nathan.r.a.schultz@gmail.com>
2025-09-28 23:53:36 -0400

    improved title screen transition and modularised title screen code

--- scenes/Background/background.gd
+++ scenes/Background/background.gd
@@ -1,6 +1,5 @@
 extends Node2D
 
-var radToTransparency := PI / 2
 var rng: RandomNumberGenerator = RandomNumberGenerator.new()
 var cloudSize: Vector2
 var activeClouds: Array[TextureRect] = []
@@ -22,12 +21,6 @@ func _ready():
 	
 	cloudSize = Vector2(36, 18)
 	initClouds()
-	
-	if Global.titleInfo[0] != -1:
-		radToTransparency = Global.titleInfo[0]
-	else:
-		$Logo.queue_free()
-		$StartInfo.queue_free()
 
 func _process(_delta):
 	
@@ -42,7 +35,7 @@ func _process(_delta):
 		isAlive = jetski.isAlive
 	
 	for layer in layers:
-		if layer[1].autoscroll[0] > -(Global.scrollSpeed * layer[0]) and main.get_node("Jetski").isAlive:
+		if layer[1].autoscroll[0] > -(Global.scrollSpeed * layer[0]) and isAlive:
 			layer[1].autoscroll[0] -= Global.scrollSpeed * _delta
 		elif Global.scrollSpeed == 0:
 			layer[1].autoscroll[0] = 0
@@ -50,18 +43,6 @@ func _process(_delta):
 			layer[1].autoscroll[0] += (Global.scrollSpeed * _delta)
 			
 		layer[1].autoscroll.x = round(layer[1].autoscroll.x)
-		
-	if Global.titleInfo[0] != -1:
-		radToTransparency += PI * _delta
-		$StartInfo.modulate = Color(1, 1, 1, (sin(radToTransparency) + 1) / 2)
-	
-		$Logo.position.x -= 2 * Global.scrollSpeed * _delta
-		$StartInfo.position.x -= 2 * Global.scrollSpeed * _delta
-		
-		if $Logo.position.x <= -$Logo.size.x + -((get_viewport_rect().size.x - 320) / 2):
-			$Logo.queue_free()
-			$StartInfo.queue_free()
-			Global.titleInfo = [-1]
 	
 func initClouds():
 	
---
+++ scenes/Background/ui.gd
@@ -0,0 +1,28 @@
+extends Control
+
+var radToTransparency := PI / 2
+
+func _ready():
+	
+	if Global.titleInfo[0] != -1:
+		radToTransparency = Global.titleInfo[0]
+	else:
+		$Logo.queue_free()
+		$StartInfo.queue_free()
+		$Credit.queue_free()
+
+func _process(_delta):
+	
+	if Global.titleInfo[0] != -1:
+		radToTransparency += PI * _delta
+		$StartInfo.modulate = Color(1, 1, 1, (sin(radToTransparency) + 1) / 2)
+	
+		$Logo.position.x -= 2 * Global.scrollSpeed * _delta
+		$StartInfo.position.x -= 2 * Global.scrollSpeed * _delta
+		$Credit.position.x -= 2 * Global.scrollSpeed * _delta
+		
+		if $Logo.position.x <= -$Logo.size.x + -((get_viewport_rect().size.x - 320) / 2):
+			$Logo.queue_free()
+			$StartInfo.queue_free()
+			$Credit.queue_free()
+			Global.titleInfo = [-1]
