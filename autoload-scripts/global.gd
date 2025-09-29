7950606c550cdaf2c665404aac735784401b149c
Natratz <nathan.r.a.schultz@gmail.com>
2025-09-28 23:29:41 -0400

    partial save data implementation

--- autoload-scripts/global.gd
+++ autoload-scripts/global.gd
@@ -1,5 +1,7 @@
 extends Node
 
+const SAVE_PATH = "user://savegame.save"
+
 var totalCoins: int
 var currentScene = null
 var jetskiInfo := [-1.0, -1.0]
@@ -55,10 +57,53 @@ func setScrollSpeed(newScrollSpeed: float):
 func getScrollSpeed():
 	return(self.scrollSpeed)
 	
-func save():
-	var save_dict = {
-		"filename" : get_scene_file_path(),
-		"parent" : get_parent().get_path(),
-		"total_coins" : totalCoins
+func saveGame():
+	var save_file = FileAccess.open(SAVE_PATH, FileAccess.WRITE)
+	if save_file == null:
+		print("error saving the game :(", FileAccess.get_open_error())
+		return
+	
+	var save_data = {
+		"totalCoins": totalCoins,
+		"currentCoins": currentCoins,
+		"distance": distance,
+		"jetskiInfo": jetskiInfo,
+		"titleInfo": titleInfo,
+		"scrollSpeed": scrollSpeed
 	}
-	return save_dict
+	
+	var json_string = JSON.stringify(save_data)
+	save_file.store_line(json_string)
+	save_file.close()
+	
+func load_game():
+	if not FileAccess.file_exists(SAVE_PATH):
+		print("no save found; creating a new save")
+		return
+	
+	var save_file = FileAccess.open(SAVE_PATH, FileAccess.READ)
+	if save_file == null:
+		print("couldn't open the file", FileAccess.get_open_error())
+		return
+	
+	var json_string = save_file.get_line()
+	save_file.close()
+	
+	var json = JSON.new()
+	var parse_result = json.parse(json_string)
+	
+	if parse_result != OK:
+		print("Error parsing save file: ", json.get_error_message())
+		return
+	
+	var save_data = json.data
+	
+	totalCoins = save_data.get("totalCoins", 0)
+	currentCoins = save_data.get("currentCoins", 0)
+	distance = save_data.get("distance", 0)
+	jetskiInfo = save_data.get("jetskiInfo", [-1.0, -1.0])
+	titleInfo = save_data.get("titleInfo", [1])
+	scrollSpeed = save_data.get("scrollSpeed", 0.0)
+	
+func hasSaveFile():
+	return FileAccess.file_exists(SAVE_PATH)
